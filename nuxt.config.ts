5e3f0ad010d035f67b311b14270f305628fea2d8
Fakhri Rossi <rossifakhri@gmail.com>
2024-07-07 18:44:01 +0700

    Fixed bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ui: {
-    icons: ['mdi', 'simple-icons', 'ph']
-  },
   modules: [ "@nuxt/ui", "@nuxtjs/tailwindcss", '@pinia/nuxt', ],
   compatibilityDate: "2024-07-04",
 
